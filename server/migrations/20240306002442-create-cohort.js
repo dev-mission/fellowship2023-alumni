16bdbda53e1c1f387388d956357e29484ad01dd4
Paolo Gaudiel <135890965+PaoloGaudiel@users.noreply.github.com>
2024-03-05 16:28:32 -0800

    Create Cohort model

---
+++ server/migrations/20240306002442-create-cohort.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Cohorts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cohortNumber: {
+        type: Sequelize.INTEGER
+      },
+      graduatedOn: {
+        type: Sequelize.DATE
+      },
+      year: {
+        type: Sequelize.STRING
+      },
+      term: {
+        type: Sequelize.STRING
+      },
+      affiliation: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Cohorts');
+  }
+};
---
+++ server/models/cohort.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cohort extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cohort.init({
+    cohortNumber: DataTypes.INTEGER,
+    graduatedOn: DataTypes.DATE,
+    year: DataTypes.STRING,
+    term: DataTypes.STRING,
+    affiliation: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Cohort',
+  });
+  return Cohort;
+};
