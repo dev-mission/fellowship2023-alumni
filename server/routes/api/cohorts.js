68db9fe48e0bfb4103cf1c1fd793a7eaf7aae793
Paolo Gaudiel <135890965+PaoloGaudiel@users.noreply.github.com>
2024-03-19 14:08:14 -0700

    Fixed initial formatting/lint issues

--- server/routes/api/cohorts.js
+++ server/routes/api/cohorts.js
@@ -3,60 +3,54 @@ import { StatusCodes } from 'http-status-codes';
 import _ from 'lodash';
 
 import models from '../../models/index.js';
-import interceptors from '../interceptors.js';
-import helpers from '../helpers.js';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    try {
-        const record = await models.Cohort.create(_.pick(req.body,
-            ['cohortNumber', 'graduatedOn', 'year', 'term', 'affiliation',
-        ]));
-        res.status(StatusCodes.CREATED).json(record.toJSON());
-    } catch (error) {
-        console.log(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-    }
+  try {
+    const record = await models.Cohort.create(_.pick(req.body, ['cohortNumber', 'graduatedOn', 'year', 'term', 'affiliation']));
+    res.status(StatusCodes.CREATED).json(record.toJSON());
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-    try {
-        const record = await models.Cohort.findByPk(req.params.id);
-        await record.destroy();
-        res.status(StatusCodes.OK).end();
-    } catch (error) {
-        console.log(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-    }
+  try {
+    const record = await models.Cohort.findByPk(req.params.id);
+    await record.destroy();
+    res.status(StatusCodes.OK).end();
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
 });
 
 router.get('/', async (req, res) => {
-    const records = await models.Cohort.findAll();
-    res.json(records.map((r) => r.toJSON()));
+  const records = await models.Cohort.findAll();
+  res.json(records.map((r) => r.toJSON()));
 });
 
 router.patch('/:id', async (req, res) => {
-    try {
-        const record = await models.Cohort.findByPk(req.params.id);
-        await record.update(_.pick(req.body,
-            ['cohortNumber', 'graduatedOn', 'year', 'term', 'affiliation',
-        ]));
-        res.json(record.toJSON());
-    } catch (error) {
-        console.log(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-    }
+  try {
+    const record = await models.Cohort.findByPk(req.params.id);
+    await record.update(_.pick(req.body, ['cohortNumber', 'graduatedOn', 'year', 'term', 'affiliation']));
+    res.json(record.toJSON());
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
 });
 
 router.get('/:id', async (req, res) => {
-    try {
-        const record = await models.Cohort.findByPk(req.params.id);
-        res.json(record.toJSON());
-    } catch (err) {
-        console.log(err);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
-    }
-})
+  try {
+    const record = await models.Cohort.findByPk(req.params.id);
+    res.json(record.toJSON());
+  } catch (err) {
+    console.log(err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
+});
 
 export default router;
--- server/test/models/cohort.js
+++ server/test/models/cohort.js
@@ -1,7 +1,4 @@
 import assert from 'assert';
-import _ from 'lodash';
-import path from 'path';
-import { v4 as uuid } from 'uuid';
 
 import helper from '../helper.js';
 import models from '../../models/index.js';
@@ -15,11 +12,11 @@ describe('models.Cohort', () => {
     assert.deepStrictEqual(await models.Cohort.count(), 2);
 
     const record = await models.Cohort.create({
-          cohortNumber: 3,
-          // graduatedOn: '2023-03-03T00:00:00.000Z',
-          year: '2023',
-          term: 'Summer',
-          affiliation: 'Dev/Mission'
+      cohortNumber: 3,
+      // graduatedOn: '2023-03-03T00:00:00.000Z',
+      year: '2023',
+      term: 'Summer',
+      affiliation: 'Dev/Mission',
     });
 
     assert.deepStrictEqual(await models.Cohort.count(), 3);
@@ -29,24 +26,23 @@ describe('models.Cohort', () => {
     assert.deepStrictEqual(record.year, '2023');
     assert.deepStrictEqual(record.term, 'Summer');
     assert.deepStrictEqual(record.affiliation, 'Dev/Mission');
-
   });
 
-  it('finds an Cohort record by its id', async() => {
+  it('finds an Cohort record by its id', async () => {
     const record = await models.Cohort.findByPk(10000);
     assert.notDeepStrictEqual(record, null);
     assert.deepStrictEqual(record.cohortNumber, 1);
   });
 
-  it('finds multiple Cohort records', async() => {
+  it('finds multiple Cohort records', async () => {
     const records = await models.Cohort.findAll({
-      order: [['cohortNumber', 'ASC']]
+      order: [['cohortNumber', 'ASC']],
     });
     assert.deepStrictEqual(records.length, 2);
     assert.deepStrictEqual(records[0].cohortNumber, 1);
   });
 
-  it('deletes a Cohort record', async() => {
+  it('deletes a Cohort record', async () => {
     assert.deepStrictEqual(await models.Cohort.count(), 2);
     const record = await models.Cohort.findByPk(10000);
     await record.destroy();
