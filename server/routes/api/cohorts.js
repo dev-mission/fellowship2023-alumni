5944865a3c9e2119f05f3e46b5deb92934580942
Paolo <135890965+PaoloGaudiel@users.noreply.github.com>
2024-03-19 19:35:16 -0700

    [#11] Added Cohort API
    * Created Cohort API without timestamp tests
    
    * Fixed initial formatting/lint issues
    
    * Included timestamp in unit tests
    
    * Fix Prettier formatting issues

---
+++ server/routes/api/cohorts.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import _ from 'lodash';
+
+import models from '../../models/index.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const record = await models.Cohort.create(_.pick(req.body, ['cohortNumber', 'graduatedOn', 'year', 'term', 'affiliation']));
+    res.status(StatusCodes.CREATED).json(record.toJSON());
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const record = await models.Cohort.findByPk(req.params.id);
+    await record.destroy();
+    res.status(StatusCodes.OK).end();
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
+});
+
+router.get('/', async (req, res) => {
+  const records = await models.Cohort.findAll();
+  res.json(records.map((r) => r.toJSON()));
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const record = await models.Cohort.findByPk(req.params.id);
+    await record.update(_.pick(req.body, ['cohortNumber', 'graduatedOn', 'year', 'term', 'affiliation']));
+    res.json(record.toJSON());
+  } catch (error) {
+    console.log(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const record = await models.Cohort.findByPk(req.params.id);
+    res.json(record.toJSON());
+  } catch (err) {
+    console.log(err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
+});
+
+export default router;
