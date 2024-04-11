55f7e97ce2f0fc8b4d5e0a03434eaa99688f1afa
sandyliu418 <sandyliu418@gmail.com>
2024-04-11 06:48:50 -0700

    Added button and continued to format buttons on opportunities page

--- client/src/Opportunities/OpportunityList.jsx
+++ client/src/Opportunities/OpportunityList.jsx
@@ -15,17 +15,25 @@ function OpportunityList() {
     <>
       <Card>
         <Card.Body>
-          <Container>
+          {/* <Container>
             <div className="row">
               <div className="col-md-6"><h1>Opportunities</h1></div>
               <div className="col-md-4 .col-md-offset-4"><Button variant="outline-primary">New Opp</Button></div>
             </div>
+          </Container> */}
+          <Container>
+            <div className="row">
+              <div className="col-md-6"><h1>Opportunities</h1></div>
+              <div className="col-md-6 col-md-offset-4"><Button className="col-md-12" variant="outline-primary">New Opp</Button></div>
+            </div>
           </Container>
-          <DropdownButton title="Recently Added" id="bg-nested-dropdown">
-            <Dropdown.Item eventKey="1">Most popular</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Newest</Dropdown.Item>
-            <Dropdown.Item eventKey="2">Oldest</Dropdown.Item>
-          </DropdownButton>
+          <div className="row">
+            <DropdownButton title="Recently Added" id="bg-nested-dropdown">
+              <Dropdown.Item eventKey="1">Most popular</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Newest</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Oldest</Dropdown.Item>
+            </DropdownButton>
+          </div>
           <Container>
             <Row>
               <Col><Button variant="outline-primary">Add Filter</Button></Col>
@@ -34,11 +42,43 @@ function OpportunityList() {
           </Container>
         </Card.Body>
       </Card>
-        <Container>
-            <div className="col-md-4 col-md-offset-5">
-              <h3>CLEAR FILTERS</h3>
-            </div>
-        </Container>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3 offset-9">
+            <h6>CLEAR FILTERS</h6>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div class="card">
+          <h5 class="card-header">Uber</h5>
+          <div class="card-body">
+            <h5 class="card-title">Software Engineer</h5>
+            <p class="card-text">San Francisco, CA</p>
+            <p class="card-text">Posted 6 days ago</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div class="card">
+          <h5 class="card-header">Uber</h5>
+          <div class="card-body">
+            <h5 class="card-title">Software Engineer</h5>
+            <p class="card-text">San Francisco, CA</p>
+            <p class="card-text">Posted 6 days ago</p>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div class="card">
+          <h5 class="card-header">Uber</h5>
+          <div class="card-body">
+            <h5 class="card-title">Software Engineer</h5>
+            <p class="card-text">San Francisco, CA</p>
+            <p class="card-text">Posted 6 days ago</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
