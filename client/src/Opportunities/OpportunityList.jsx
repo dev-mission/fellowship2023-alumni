4aa3aa78611d4f40b6d2fee4506cdc2367d4027b
sandyliu418 <sandyliu418@gmail.com>
2024-04-17 07:12:45 -0700

    all elements of Opportunities-List view have been added

--- client/src/Opportunities/OpportunityList.jsx
+++ client/src/Opportunities/OpportunityList.jsx
@@ -10,10 +10,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Badge from 'react-bootstrap/Badge';
+
 function OpportunityList() {
   return (
     <>
-      <Card>
         <Card.Body>
           {/* <Container>
             <div className="row">
@@ -27,13 +28,23 @@ function OpportunityList() {
               <div className="col-md-6 col-md-offset-4"><Button className="col-md-12" variant="outline-primary">New Opp</Button></div>
             </div>
           </Container>
-          <div className="row">
-            <DropdownButton title="Recently Added" id="bg-nested-dropdown">
+          <Container>
+            <div className="row">
+              <div className="d-flex">
+                {/* <DropdownButton className="w-100" title="Recently Added" id="bg-nested-dropdown">
               <Dropdown.Item eventKey="1">Most popular</Dropdown.Item>
               <Dropdown.Item eventKey="2">Newest</Dropdown.Item>
               <Dropdown.Item eventKey="2">Oldest</Dropdown.Item>
-            </DropdownButton>
-          </div>
+            </DropdownButton> */}
+                <Form.Select aria-label="Default select example">
+                  <option>Recently Added</option>
+                  <option value="1">Most popular</option>
+                  <option value="2">Newest</option>
+                  <option value="3">Oldest</option>
+                </Form.Select>
+              </div>
+            </div>
+          </Container>
           <Container>
             <Row>
               <Col><Button variant="outline-primary">Add Filter</Button></Col>
@@ -41,7 +52,6 @@ function OpportunityList() {
             </Row>
           </Container>
         </Card.Body>
-      </Card>
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-3 offset-9">
@@ -49,36 +59,44 @@ function OpportunityList() {
           </div>
         </div>
       </div>
-      <div className="container-fluid">
-        <div class="card">
-          <h5 class="card-header">Uber</h5>
-          <div class="card-body">
-            <h5 class="card-title">Software Engineer</h5>
-            <p class="card-text">San Francisco, CA</p>
-            <p class="card-text">Posted 6 days ago</p>
+      <Container>
+        <div className="card">
+          <h5 className="card-header d-flex justify-content-between">Uber
+            <Badge pill bg="primary">
+              Jobs
+            </Badge>
+          </h5>
+          <div className="card-body">
+            <h5 className="card-title">Software Engineer</h5>
+            <p className="card-text">San Francisco, CA</p>
+            <p className="card-text">Posted 6 days ago</p>
           </div>
         </div>
-      </div>
-      <div className="container-fluid">
-        <div class="card">
-          <h5 class="card-header">Uber</h5>
-          <div class="card-body">
-            <h5 class="card-title">Software Engineer</h5>
-            <p class="card-text">San Francisco, CA</p>
-            <p class="card-text">Posted 6 days ago</p>
+          <div className="card">
+            <h5 className="card-header d-flex justify-content-between">Uber
+              <Badge pill bg="primary">
+                Jobs
+              </Badge>
+            </h5>
+            <div className="card-body">
+              <h5 className="card-title">Software Engineer</h5>
+              <p className="card-text">San Francisco, CA</p>
+              <p className="card-text">Posted 6 days ago</p>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="container-fluid">
-        <div class="card">
-          <h5 class="card-header">Uber</h5>
-          <div class="card-body">
-            <h5 class="card-title">Software Engineer</h5>
-            <p class="card-text">San Francisco, CA</p>
-            <p class="card-text">Posted 6 days ago</p>
+          <div className="card">
+            <h5 className="card-header d-flex justify-content-between">Uber
+              <Badge pill bg="primary">
+                Jobs
+              </Badge>
+            </h5>
+            <div className="card-body">
+              <h5 className="card-title">Software Engineer</h5>
+              <p className="card-text">San Francisco, CA</p>
+              <p className="card-text">Posted 6 days ago</p>
+            </div>
           </div>
-        </div>
-      </div>
+      </Container>
     </>
   );
 }
