17f916ca80f8bfce06f09049653ee00dcc91bfe7
Paolo Gaudiel <135890965+PaoloGaudiel@users.noreply.github.com>
2024-04-29 13:37:44 -0700

    Added Tag filter capability

--- client/src/Opportunities/OpportunityList.jsx
+++ client/src/Opportunities/OpportunityList.jsx
@@ -13,17 +13,52 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import Modal from 'react-bootstrap/Modal';
 import Badge from 'react-bootstrap/Badge';
 
 function OpportunityList() {
   const navigate = useNavigate();
   const staticContext = useStaticContext();
   const [posts, setPosts] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTags, setSelectedTags] = useState([]);
 
   useEffect(() => {
     Api.posts.index().then((response) => setPosts(response.data));
   }, []);
+  useEffect(() => {
+    Api.tags.index().then((response) => setTags(response.data));
+  }, []);
+
+  const filteredPosts = posts.filter(
+    (post) =>
+      (post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        (post.Organization && post.Organization.name.toLowerCase().includes(searchTerm.toLowerCase())) ||
+        (post.workLocation && post.workLocation.toLowerCase().includes(searchTerm.toLowerCase()))) &&
+      selectedTags.every((tagId) => post.Tags.some((tag) => tag.id === tagId)),
+  );
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleTagSelection = (tagId) => {
+    if (selectedTags.includes(tagId)) {
+      setSelectedTags(selectedTags.filter((id) => id !== tagId));
+    } else {
+      setSelectedTags([...selectedTags, tagId]);
+    }
+  };
+
+  const clearFilters = () => {
+    setSelectedTags([]);
+  };
 
   async function onDelete(event, postId) {
     event.stopPropagation();
@@ -46,11 +81,11 @@ function OpportunityList() {
       </Helmet>
       <div className="posts container">
         <Container>
-          <div className="row">
-            <div className="col-md-6">
+          <div className="row d-flex justify-content-between">
+            <div className="col">
               <h1>Opportunities</h1>
             </div>
-            <div className="col-md-6 col-md-offset-4">
+            <div className="col">
               <Link to="new">
                 <Button className="col-md-12" variant="outline-primary">
                   New Opp
@@ -62,34 +97,81 @@ function OpportunityList() {
         <Container>
           <div className="row">
             <div className="d-flex">
-              <Form.Select aria-label="Default select example">
+              <select className="form-select mb-3">
                 <option>Recently Added</option>
                 <option value="1">Most popular</option>
                 <option value="2">Newest</option>
                 <option value="3">Oldest</option>
-              </Form.Select>
+              </select>
             </div>
           </div>
         </Container>
         <Container>
           <Row>
             <Col>
-              <Button variant="outline-primary">Add Filter</Button>
+              <Container>
+                {/* Filter Modal Button */}
+                <Button variant="outline-primary" onClick={toggleModal}>
+                  Add Filter
+                </Button>
+                {/* Filter Modal */}
+                <Modal show={showModal} onHide={toggleModal}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Filter by Tags</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    {tags.map((tag) => (
+                      <Form.Check
+                        key={tag.id}
+                        type="checkbox"
+                        label={tag.name}
+                        checked={selectedTags.includes(tag.id)}
+                        onChange={() => handleTagSelection(tag.id)}
+                      />
+                    ))}
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={toggleModal}>
+                      Close
+                    </Button>
+                    <Button variant="primary" onClick={toggleModal}>
+                      Apply Filters
+                    </Button>
+                  </Modal.Footer>
+                </Modal>
+              </Container>
             </Col>
             <Col>
-              <Form.Control type="text" placeholder="Search" />
+              <Form>
+                <Form.Control type="text" placeholder="Search" value={searchTerm} onChange={handleSearchChange} />
+              </Form>
             </Col>
           </Row>
         </Container>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-3 offset-9">
-              <h6>CLEAR FILTERS</h6>
-            </div>
-          </div>
-        </div>
         <Container>
-          {posts.map((post) => (
+          <Row>
+            <div className="col-8">
+              <div className="d-flex flex-nowrap overflow-auto">
+                {selectedTags.map((tagId) => {
+                  const tag = tags.find((tag) => tag.id === tagId);
+                  return (
+                    <Badge key={tag.id} variant="primary" className="mr-2">
+                      {tag.name}
+                    </Badge>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="col-4">
+              <Button variant="link" onClick={clearFilters}>
+                CLEAR FILTERS
+              </Button>
+            </div>
+          </Row>
+        </Container>
+
+        <Container>
+          {filteredPosts.map((post) => (
             <div key={post.id} onClick={() => navigate(`${post.id}`)} className="card mb-3">
               <h5 className="card-header d-flex justify-content-between">
                 <div>
@@ -111,8 +193,8 @@ function OpportunityList() {
               </h5>
               <div className="card-body">
                 <h5 className="card-title">{post.title}</h5>
-                <p className="card-text">{post.location}</p>
-                <p className="card-text">{new DateTime(post.createdAt).diffNow('days').toFormat('d') + ' days ago'}</p>
+                <p className="card-text">{post.workLocation}</p>
+                <p className="card-text">{'Posted ' + new DateTime(post.createdAt).diffNow('days').toFormat('d') + ' days ago'}</p>
               </div>
             </div>
           ))}
