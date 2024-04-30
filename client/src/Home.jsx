9abdb3d700ee12e171107745792a91dcea83c55f
Paolo Gaudiel <135890965+PaoloGaudiel@users.noreply.github.com>
2024-04-29 20:18:12 -0700

    Demo ready with hard-coded home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import { useStaticContext } from './StaticContext';
 import photo from '../public/photos/MartinHelping.png';
+import Container from 'react-bootstrap/Container';
 
 function Home() {
   const staticContext = useStaticContext();
@@ -9,30 +10,30 @@ function Home() {
       <Helmet>
         <title>Home - {staticContext?.env?.VITE_SITE_TITLE ?? ''}</title>
       </Helmet>
-      <main className="container">
+      <Container>
         <h1>Home</h1>
-        <div className="card">
+        <div className="card mb-3">
           <img src={photo} className="card-img-top" alt="Dev/Mission Highlights"></img>
-            <div className="card-body">
-              <h5 className="card-title">Spotlight: Martin Tran</h5>
-              <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
-            </div>
+          <div className="card-body">
+            <h5 className="card-title">Spotlight: Martin Tran</h5>
+            <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
+          </div>
         </div>
-        <div className="card">
+        <div className="card mb-3">
           <img src={photo} className="card-img-top" alt="Dev/Mission Highlights"></img>
-            <div className="card-body">
-              <h5 className="card-title">Spotlight: Martin Tran</h5>
-              <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
-            </div>
+          <div className="card-body">
+            <h5 className="card-title">Spotlight: Martin Tran</h5>
+            <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
+          </div>
         </div>
-        <div className="card">
+        <div className="card mb-3">
           <img src={photo} className="card-img-top" alt="Dev/Mission Highlights"></img>
-            <div className="card-body">
-              <h5 className="card-title">Spotlight: Martin Tran</h5>
-              <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
-            </div>
+          <div className="card-body">
+            <h5 className="card-title">Spotlight: Martin Tran</h5>
+            <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
+          </div>
         </div>
-      </main>
+      </Container>
     </>
   );
 }
