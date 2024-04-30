51a21edc74b6a81ce60a236088dee656cde26d07
Paolo <135890965+PaoloGaudiel@users.noreply.github.com>
2024-04-29 20:22:35 -0700

    [Closes #48] Home page hard coded (#49)
    * Create Cohort model
    
    * Create Bookmark model
    
    * Create Post model
    
    * Create PostTag model
    
    * Create Tag model
    
    * Create Organization model
    
    * Create Program model
    
    * Fix styling issues
    
    * Established foreign key references
    
    * Created associations between models
    
    * Fix formatting and linting
    
    * Added Spotlight cards to Home Page
    
    * Fix formatting in home-page
    
    * Revert "Fix formatting in home-page"
    
    This reverts commit ab09e238ead4661f14063b8d03da49ef4042edbb.
    
    * Demo ready with hard-coded home page
    
    ---------
    
    Co-authored-by: sandyliu418 <sandyliu418@gmail.com>

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,5 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import { useStaticContext } from './StaticContext';
+import photo from '../public/photos/MartinHelping.png';
+import Container from 'react-bootstrap/Container';
 
 function Home() {
   const staticContext = useStaticContext();
@@ -8,9 +10,30 @@ function Home() {
       <Helmet>
         <title>Home - {staticContext?.env?.VITE_SITE_TITLE ?? ''}</title>
       </Helmet>
-      <main className="container">
+      <Container>
         <h1>Home</h1>
-      </main>
+        <div className="card mb-3">
+          <img src={photo} className="card-img-top" alt="Dev/Mission Highlights"></img>
+          <div className="card-body">
+            <h5 className="card-title">Spotlight: Martin Tran</h5>
+            <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <img src={photo} className="card-img-top" alt="Dev/Mission Highlights"></img>
+          <div className="card-body">
+            <h5 className="card-title">Spotlight: Martin Tran</h5>
+            <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <img src={photo} className="card-img-top" alt="Dev/Mission Highlights"></img>
+          <div className="card-body">
+            <h5 className="card-title">Spotlight: Martin Tran</h5>
+            <p className="card-text">A Dev/Mission Fellowship Alumni giving back to the community</p>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
